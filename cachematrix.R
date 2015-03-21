12a3749a9600e75d46c7b097c547ff8007589e52
Bushwa <cervolles@gmail.com>
2015-03-21 21:39:13 +0000

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions returning objects to allow memoization of a matrix inverse  
 
-## Write a short comment describing this function
+## Returns object storing an invertible matrix and its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(myMatrix = matrix()) {
+	    myMatrixInverse <- NULL
+            set <- function(mat) {
+	    	myMatrix <<- mat
+	    	myMatrixInverse <<- NULL
+	            }
+	    get <- function() myMatrix
+	    setInverse <- function(inverse) myMatrixInverse <<- inverse
+	    getInverse <- function() myMatrixInverse
+	    
+	    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
+## Takes a matrix object X and computes inverse storing it in X's inverse cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	    inverse <- x$getInverse()
+            if(!is.null(inverse)) {
+            	message("getting cached data")
+	        return(inverse)  
+	    }
+	    mat <- x$get()
+	    inverse <- solve(mat)
+	    x$setInverse(inverse)
+            inverse
 }
+
+makeCacheMatrixImproved <- function(myMatrix = matrix()) {
+	    myMatrixInverse <- NULL
+            set <- function(mat) {
+		                    myMatrix <<- mat
+	                    myMatrixInverse <<- NULL
+			                        }
+	                get <- function() myMatrix
+	                getInverse <- function() {
+				                if (is.null(myMatrixInverse)) {
+							                    myMatrixInverse <<- solve(myMatrix)
+			                }
+			                myMatrixInverse
+					            }
+			            list(set = set, get = get, getInverse = getInverse)
+
+
+}
+
